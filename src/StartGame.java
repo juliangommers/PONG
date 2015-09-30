d47533f4c75c4422de63f2b582298b59bec81664
Julian Gommers <j.gommers@student.hhs.nl>
2015-09-30 15:44:16 +0200

    The block moves 😍

--- src/StartGame.java
+++ src/StartGame.java
@@ -6,6 +6,7 @@ import org.newdawn.slick.BasicGame;
 import org.newdawn.slick.Color;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Input;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.geom.Rectangle;
 import org.newdawn.slick.geom.Shape;
@@ -14,6 +15,8 @@ public class StartGame extends BasicGame
 {
 	
 	private Shape rect = null;
+	private int xP1 = 100;
+	private int yP1 = 100;
 	
 	public StartGame(String gamename)
 	{
@@ -22,19 +25,27 @@ public class StartGame extends BasicGame
 
 	@Override
 	public void init(GameContainer container) throws SlickException {
-		container.setShowFPS(false);
-		rect = new Rectangle(100,100,25,25);
+		container.setShowFPS(true);
+		rect = new Rectangle(xP1,yP1,25,25);
 	}
 
 	@Override
 	public void update(GameContainer container, int delta) throws SlickException {
-//		input.isKeyDown(Input.KEY_SPACE);
+		Input input = container.getInput();
+		if( input.isKeyDown(Input.KEY_DOWN)  && rect.getY() <= (container.getHeight()-rect.getHeight()) ){
+			yP1++;
+			rect.setLocation(xP1, yP1);
+		}
+		if( input.isKeyDown(Input.KEY_UP) && rect.getY() >= 0 ){
+			yP1--;
+			rect.setLocation(xP1, yP1);
+		}
+			
 	}
 
 	@Override
 	public void render(GameContainer container, Graphics g) throws SlickException {
 		
-		g.setColor(new Color(255,255,0));
 		g.draw(rect);
 		g.setColor(new Color(255,255,255));
 		g.fill(rect);
